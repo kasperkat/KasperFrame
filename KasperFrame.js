b97402dc2bfdd0a27f5b060ac982d318b6ae7be4
kasperkat <kasperkat666@gmail.com>
2022-06-15 19:09:05 -0700

    core functionality working. motor can now be rotated from webapp, and arduino reports the status. (moving/idle)

--- KasperFrame.js
+++ KasperFrame.js
@@ -1,3 +1,122 @@
+const { SerialPort, ReadlineParser } = require('serialport')
+const port = new SerialPort({ path: 'COM4', baudRate: 115200 })
+const parser = new ReadlineParser();
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const http_port = process.env.PORT || "8000";
+
+
+
+content = "";
+machineStatus = "";
+
+// Serial communication
+port.pipe(parser);
+
+ready = false;
+connected = false;
+requestingData = false;
+step = 0;
+
+port.on('data', function (data) {
+  
+  // console.log(data.toString().indexOf("ok"))
+
+  ready = true;
+
+  if(requestingData){
+
+    if(data.indexOf("Idle") >= 0){
+      machineStatus = "Idle";
+    }else if(data.indexOf("Run") >= 0 ){
+      machineStatus = "Moving";
+    }
+    console.log(machineStatus);
+    requestingData = false;
+  }
+
+
+
+  
+})
+
+function onConnect(){
+   
+  if(ready){
+    port.write( content );
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+// webapp
+app.get("/", (req, res) => {
+  // res.status(200).send("WHATABYTE: Food For Devs");
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+
+// app.post('/', (req, res) => {
+//   onConnect();
+//   res.sendFile(path.join(__dirname, '/index.html'));
+// })
+
+app.post('/', (req, res) => {
+
+
+  onConnect();
+
+  const machineStatusStream = setInterval(getMachineStatus, 1000);
+
+  function getMachineStatus() {
+
+    if(machineStatus == "Idle"){
+      console.log("STOP!");
+      machineStatus = "";
+      stopStatusStream();
+    }else{
+      requestingData = true;
+      port.write("?\n");
+    }
+
+
+
+  }
+  
+  function stopStatusStream() {
+    clearInterval(machineStatusStream);
+  }
+
+  
+
+
+  res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+
+
+
+app.listen(http_port, () => {
+  console.log(`Listening to requests on http://localhost:${http_port}`);
+});
+
+
+
+
+
+
+// gcode generator
+
 
 const fs = require ('fs');
 
@@ -18,6 +137,7 @@ for( i = 0; i <= (length / step); i ++ ){
 
 }
 content += "G1 X0 F6000\n";
+
 
 
 function speedCurve(pos, inSpeed, limit){
@@ -41,17 +161,8 @@ fs.writeFile('test.gcode', content, err => {
     // file written successfully
   });
   
-
-
-
-
-
-
-
-
-
-
-
-
-
 console.log("hello world");
+
+
+
+
